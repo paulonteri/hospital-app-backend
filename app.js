c66c3d7a0534ddbdf2ee563c3f988ee3cc3306b1
Paul Onteri <onteripaul@gmail.com>
2020-06-24 19:38:04 +0300

    basic APIs

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+var bodyParser = require("body-parser");
+// var upload = multer(); // for parsing multipart/form-data
 
 const doctors = require("./models/doctors");
+const hospitals = require("./models/hospitals");
+const location = require("./models/location");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.json());
 
 app.listen(port, () =>
   console.log(`Hospital Backend listening at http://localhost:${port}`)
 );
 
+app.get("/", (req, res) => res.send("Hi There!"));
+
 app.get("/api/listdoctors", function (req, res) {
-  console.log("/api/listdoctors");
+  console.log("Hit");
   res.json(doctors.doctors);
+});
+
+app.get("/api/listcounties", function (req, res) {
+  console.log("Hit");
+  res.json(location.counties);
+});
+
+app.get("/api/listhospitals/", function (req, res) {
+  console.log("Hit");
+  res.json(hospitals.hospitals);
+});
+
+app.get("/api/listspecialization/", function (req, res) {
+  console.log("Hit");
+  res.json(doctors.specialization);
+});
+
+app.post("/api/appointments/book/", function (req, res) {
+  req.accepts("json");
+  console.log("Hit");
+  console.log(req.body);
+  console.log(req.params);
+  res.end();
 });
