01361ac456de10c79fea1f026adfe442653bfac6
Paul Onteri <onteripaul@gmail.com>
2020-06-29 11:06:37 +0300

    Added auth models

---
+++ models/auth.js
@@ -0,0 +1,12 @@
+const user = {
+  myPatient: {
+    email: "onteripaul@gmail.com",
+    firstName: "First Name",
+    middleName: "Middle Name",
+    lastName: "Last Name",
+    id: "343434",
+    idPassport: 23232323,
+  },
+};
+
+module.exports = { user: user };
