6f0fbe6418440250169245239f98bb773ed8320b
Paul Onteri <onteripaul@gmail.com>
2020-07-06 18:41:17 +0300

    passport setup

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const session = require("express-session");
 
-// Databse
-db = require("./config/database");
-
+const db = require("./config/database");
 const port = 3000;
-
 const app = express();
 
+// Database connection
 db.authenticate()
   .then(() =>
     console.log(
@@ -16,12 +16,19 @@ db.authenticate()
   )
   .catch((err) => console.error("Unable to connect to the database:", err));
 
+// For passport
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
 app.use(bodyParser.json());
 app.listen(port, () =>
   console.log(`Hospital Backend listening at http://localhost:${port}`)
 );
 
+// ROUTES
 app.get("/", (req, res) => res.send("Hi There!"));
-
 // API routes
 app.use("/api", require("./routes/api"));
