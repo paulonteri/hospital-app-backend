a2992faf34d41cbc867eaac486200a3649e94000
Paul Onteri <onteripaul@gmail.com>
2020-07-06 19:14:45 +0300

    Hospital model

--- migrations/20200706082341-create-user.js
+++ migrations/20200706082341-create-user.js
@@ -3,6 +3,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable("Users", {
       id: {
+        allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
--- models/hospital.js
+++ models/hospital.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Hospital extends Model {
     /**
@@ -12,13 +10,24 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Hospital.init({
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Hospital',
-  });
+  }
+  Hospital.init(
+    {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "Hospital",
+    }
+  );
   return Hospital;
 };
