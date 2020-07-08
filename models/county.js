3b1203893233bca583b5269048b7f633804ba82d
Paul Onteri <onteripaul@gmail.com>
2020-07-08 18:13:00 +0300

    Added counties model

---
+++ models/county.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class County extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  County.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "County",
+      unique: true,
+      allowNull: false,
+    }
+  );
+  return County;
+};
