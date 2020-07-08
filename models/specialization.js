5044810087429e8a514dff3171c85bf7524ce115
Paul Onteri <onteripaul@gmail.com>
2020-07-08 18:25:45 +0300

    Added specialization model

---
+++ models/specialization.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Specialization extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Specialization.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Specialization",
+      unique: true,
+      allowNull: false,
+    }
+  );
+  return Specialization;
+};
