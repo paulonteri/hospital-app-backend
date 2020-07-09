f9c9ffdddd047e2aa3b27f0ae13f87b08177aa0c
Paul Onteri <onteripaul@gmail.com>
2020-07-09 21:52:49 +0300

    warp app in db connection

--- app.js
+++ app.js
@@ -10,30 +10,30 @@ const { User } = require("./models/index");
 
 // Database connection
 db.authenticate()
-  .then(() =>
+  .then(() => {
     console.log(
       "A connection to the database has been established successfully."
-    )
-  )
+    );
+    app.use(bodyParser.json());
+    // For passport
+    app.use(
+      session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+    ); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login
+    app.listen(port, () =>
+      console.log(`Hospital Backend listening at http://localhost:${port}`)
+    );
+
+    //
+    // ROUTES
+    app.get("/", (req, res) => res.send("Hi There!"));
+    // API routes
+    app.use("/api", require("./routes/api"));
+
+    // Authentication Routes
+    require("./routes/auth.js")(app, passport);
+    //load passport strategies
+    require("./config/passport/passport.js")(passport, User);
+  })
   .catch((err) => console.error("Unable to connect to the database:", err));
-app.use(bodyParser.json());
-// For passport
-app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login
-app.listen(port, () =>
-  console.log(`Hospital Backend listening at http://localhost:${port}`)
-);
-
-//
-// ROUTES
-app.get("/", (req, res) => res.send("Hi There!"));
-// API routes
-app.use("/api", require("./routes/api"));
-
-// Authentication Routes
-require("./routes/auth.js")(app, passport);
-//load passport strategies
-require("./config/passport/passport.js")(passport, User);
