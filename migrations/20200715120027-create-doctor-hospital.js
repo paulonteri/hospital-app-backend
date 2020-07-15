79555505f203b6cb304cebdbe9fe2b5e4f0930b9
Paul Onteri <onteripaul@gmail.com>
2020-07-15 15:04:58 +0300

    doctor hospital model and migrations

---
+++ migrations/20200715120027-create-doctor-hospital.js
@@ -0,0 +1,41 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("DoctorHospitals", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      doctorId: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: "Doctors",
+          key: "id",
+        },
+        allowNull: false,
+        onDelete: "cascade",
+      },
+      hospitalId: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: "Hospitals",
+          key: "id",
+        },
+        allowNull: false,
+        onDelete: "cascade",
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("DoctorHospitals");
+  },
+};
---
+++ models/doctorhospital.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class DoctorHospital extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      // doctorId
+      this.belongsTo(models.Doctor, {
+        onDelete: "CASCADE",
+        as: "doctor",
+        foreignKey: {
+          allowNull: false,
+        },
+      });
+
+      // hospitalId
+      this.belongsTo(models.Hospital, {
+        onDelete: "CASCADE",
+        as: "hospital",
+        foreignKey: {
+          allowNull: false,
+        },
+      });
+    }
+  }
+  DoctorHospital.init(
+    {},
+    {
+      sequelize,
+      modelName: "DoctorHospital",
+    }
+  );
+  return DoctorHospital;
+};
